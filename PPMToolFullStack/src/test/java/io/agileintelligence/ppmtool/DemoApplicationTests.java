21ac1e9c3878b86740edb216b9620c4803fb63a8
zendge89 <backend.developer89@gmail.com>
2020-02-09 16:39:30 +0530

    First Commit

---
+++ PPMToolFullStack/src/test/java/io/agileintelligence/ppmtool/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
